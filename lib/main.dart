45684b68b67d0e0ab8b40b0f84a9298802459ab4
zentaza <near17e@gmail.com>
2025-09-16 22:24:06 +0700

    feat: add error widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,28 @@ import 'package:flutter/services.dart';
 import 'package:sizer/sizer.dart';
 
 import '../core/app_export.dart';
-// import '../widgets/custom_error_widget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // bool _hasShownError = false;
+  bool _hasShownError = false;
 
   // 🚨 CRITICAL: Custom error handling - DO NOT REMOVE
-  // ErrorWidget.builder = (FlutterErrorDetails details) {
-  //   if (!_hasShownError) {
-  //     _hasShownError = true;
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    if (!_hasShownError) {
+      _hasShownError = true;
 
-  //     // Reset flag after 3 seconds to allow error widget on new screens
-  //     Future.delayed(Duration(seconds: 5), () {
-  //       _hasShownError = false;
-  //     });
+      // Reset flag after 3 seconds to allow error widget on new screens
+      Future.delayed(Duration(seconds: 5), () {
+        _hasShownError = false;
+      });
 
-  //     return CustomErrorWidget(
-  //       errorDetails: details,
-  //     );
-  //   }
-  //   return SizedBox.shrink();
-  // };
+      return CustomErrorWidget(
+        errorDetails: details,
+      );
+    }
+    return SizedBox.shrink();
+  };
 
   // 🚨 CRITICAL: Device orientation lock - DO NOT REMOVE
   Future.wait([
